778d6f600fd97c3639ffc8d71be29b88c61a8b5e
Shrijal Tamrakar <tamrakarshrijal@gmail.com>
2020-02-04 00:12:16 +0545

    Add Card Component with image, name and email of monster

--- src/component/card-list/card-list.component.jsx
+++ src/component/card-list/card-list.component.jsx
@@ -1,7 +1,15 @@
 import React from "react";
-import './card-list.style.css'
+import { Card } from "../card/card.component";
 
-export const CardList = (props) => {
-  console.log(props)
-  return <div className="card-list">{props.children}</div>;
+import "./card-list.style.css";
+
+export const CardList = props => {
+  console.log(props);
+  return (
+    <div className="card-list">
+      {props.monsters.map(monster => (
+        <Card key={monster.id} monster={monster} />
+      ))}
+    </div>
+  );
 };
