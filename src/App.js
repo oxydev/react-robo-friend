5068bf4e3b5b894111598ba38b6c68aac36e0424
Shrijal Tamrakar <tamrakarshrijal@gmail.com>
2020-02-09 00:59:30 +0545

    Add search field to the app -36

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React, { Component } from 'react';
-import { CardList } from './component/card-list/card-list.component'
-import './App.css';
+import React, { Component } from "react";
+import { CardList } from "./component/card-list/card-list.component";
+import "./App.css";
+import { SearchBox } from "./component/search-box/search-box.component";
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      monsters: []
-    }
+      monsters: [],
+      searchField: ""
+    };
   }
-
+  
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
-      .then(response =>
-        response.json())
-      .then(users => this.setState({ monsters: users }))
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
   }
 
   render() {
     return (
       <div className="App">
+        <SearchBox
+          placeholder="search monster"
+          handleChange={e => {
+            this.setState({ searchField: e.target.value }, () => {
+              console.log(this.state);
+            });
+          }}
+        />
         <CardList monsters={this.state.monsters} />
       </div>
     );
