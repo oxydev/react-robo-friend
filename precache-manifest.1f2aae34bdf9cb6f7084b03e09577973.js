43b2f09b733e842cae4493f64ff2414f3c08b64c
Shrijal Tamrakar <tamrakarshrijal@gmail.com>
2020-03-28 21:59:19 +0545

    Updates

---
+++ precache-manifest.1f2aae34bdf9cb6f7084b03e09577973.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a23c31fee7c6fd3be5229e6f77877ea",
+    "url": "/react-robo-friend/index.html"
+  },
+  {
+    "revision": "d569ecca435aaf4a8e9c",
+    "url": "/react-robo-friend/static/css/main.5cfa05cf.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/react-robo-friend/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-robo-friend/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d569ecca435aaf4a8e9c",
+    "url": "/react-robo-friend/static/js/main.43103392.chunk.js"
+  },
+  {
+    "revision": "00ba74b5e9565c060586",
+    "url": "/react-robo-friend/static/js/runtime-main.2e0f6239.js"
+  }
+]);
